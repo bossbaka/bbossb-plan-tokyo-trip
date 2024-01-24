29835a389cd9b6d66103da67acdeaf9caa8fad8c
bossbaka <phisek.boss@gmail.com>
2024-01-24 21:15:02 +0700

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.82h2po06cc"
+    "revision": "0.1qsegu7m48g"
   }], {
     "directoryIndex": "index.html"
   });
